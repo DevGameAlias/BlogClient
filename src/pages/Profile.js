1d1b54326056a1cf8ef55eab6c3133705d427829
Rick <166161351+DevGameAlias@users.noreply.github.com>
2024-10-28 19:27:10 -0400

    create profile page to create crud

---
+++ src/pages/Profile.js
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+const Profile = () =>{
+    return(
+        <>
+        <div className="blog">
+        {/* Insertn Blog Crud here */}
+        </div>
+        <div className="short-story">
+            {/* Insert Short story CRUD here */}
+        </div>
+
+        </>
+    )
+}
