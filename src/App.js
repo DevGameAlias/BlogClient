ed72d357cde8800098563a6a84088e2bba09ab0c
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2024-11-04 18:43:06 -0500

    added Route and import for about page

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import {
 } from "react-router-dom";
 import AppLayout from "./appLayout/AppLayout";
 import Profile from './pages/Profile'; // Import your Profile component
+import AboutPage from "./pages/AboutPage";
 
 // Routes for the main application
 // "/blogCreation" renders the Profile component
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<AppLayout />}>
     <Route path="blogCreation" element={<Profile />} />
-    <Route path="blogList" element={<Profile />} />
+    <Route path="blogList" element={<Profile />} />   
+    <Route path="/about" element={<AboutPage />} />
   </Route>
 ));
 
