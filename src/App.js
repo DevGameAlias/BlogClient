36d7a4c73960ed3e03761c034a28b1b38ef14071
Rick <166161351+DevGameAlias@users.noreply.github.com>
2024-11-06 10:33:37 -0500

    note out login import and route as login component was created in another tree

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import {
 
 
 import AppLayout from "./appLayout/AppLayout";
-import Login from './components/Login';
+// import Login from './components/Login';
 
 
 //Routes for the main application
 const  router= createBrowserRouter(createRoutesFromElements((
   <Route path="/" element={<AppLayout />} >
   <Route path ="/short-stories" element = {<Profile />} />
-  <Route path ="/login" element = {<Login />} />
+  {/* <Route path ="/login" element = {<Login />} /> */}
   </Route>
 )))
 
