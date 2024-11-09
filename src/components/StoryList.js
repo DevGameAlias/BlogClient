df02dbab168301216af50b91913bc5e96ec17429
Gmrvofficial <samuelbinvest@outlook.com>
2024-11-09 15:07:49 -0500

    Render the pop up button

--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -35,7 +35,7 @@ const StoryList = () => {
       <div className="flex flex-wrap gap-5">
         {/* .MAP() creates a new array by transforming the elements of an existing array */}
         {stories.map((story) => (
-          
+
           <div
             key={story._id}
             className="bg-white border border-gray-300 rounded-lg p-5 shadow-md w-72"
@@ -44,9 +44,12 @@ const StoryList = () => {
             <p className="text-gray-700">{story.content}</p>
             {/* adds button on each story that is array casted */}
             
-            <button onClick={()=>setDeletedpost(story._id)}>{/*Set the story ID for deletion */}
-              DELETE
+            <button onClick={()=>setDeletedpost(story._id)} className="bg-amber-400 rounded-md shadow-lg p-1 text-sm inset-y-0.5 hover:animate-pulse">{/*Set the story ID for deletion */}
+              <div className=""> DELETE </div>
             </button>
+
+            {/* pass through props POSTID & onDelete */} 
+            {postIdDelete === story._id && <DeleteStory postId={postIdDelete} onDelete={handleStoryDelete}/>}
           </div>
 
         ))}
