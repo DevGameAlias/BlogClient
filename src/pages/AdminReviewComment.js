856e6b434caf150d5d36843aef373eb3cb0d54b8
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2024-11-13 10:25:56 -0500

    updated respone to lg and smal screen size so the comments and reviews they will stack on top of each other

--- src/pages/AdminReviewComment.js
+++ src/pages/AdminReviewComment.js
@@ -8,15 +8,15 @@ const AdminReviewComment = () => {
       <h1 className="text-3xl font-bold mb-6">ALL Blog Comments And Short Story Reviews</h1>
 
       {/* Flexbox container to arrange Comments and Reviews side by side */}
-      <div className="flex gap-8">
+      <div className="flex flex-col lg:flex-row gap-8">
         {/* Left Section for Comments */}
-        <div className="flex-1 bg-gray-100 p-4 rounded-lg shadow-lg">
+        <div className="w-full lg:w-1/2 bg-gray-100 p-4 rounded-lg shadow-lg">
           <h2 className="text-2xl font-semibold mb-4">Comments Section</h2>
           <AdminComments /> {/* Render the AdminComments component */}
         </div>
 
         {/* Right Section for Reviews */}
-        <div className="flex-1 bg-white p-4 rounded-lg shadow-lg">
+        <div className="w-full lg:w-1/2  bg-white p-4 rounded-lg shadow-lg">
           <h2 className="text-2xl font-semibold mb-4">Reviews Section</h2>
           <AdminReviews /> {/* Render the AdminReviews component */}
         </div>
