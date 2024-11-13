b8ece1fe2f9adf4f98fbe1a135c89cc00eb2a010
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2024-11-13 12:56:17 -0500

    added maching styles

--- src/pages/AdminReviewComment.js
+++ src/pages/AdminReviewComment.js
@@ -4,20 +4,20 @@ import AdminComments from '../components/AdminComments';  // Import the AdminCom
 
 const AdminReviewComment = () => {
   return (
-    <div className="admin-dashboard p-6">
-      <h1 className="text-3xl font-bold mb-6">ALL Blog Comments And Short Story Reviews</h1>
+    <div className="admin-dashboard bg-white ">
+      <h1 className="text-3xl text-yellow-950 text-center font-bold mb-6"></h1>
 
       {/* Flexbox container to arrange Comments and Reviews side by side */}
       <div className="flex flex-col lg:flex-row gap-8">
         {/* Left Section for Comments */}
-        <div className="w-full lg:w-1/2 bg-gray-100 p-4 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-semibold mb-4">Comments Section</h2>
+        <div className="w-full lg:w-1/2 bg--100 p-4 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-semibold mb-4"></h2>
           <AdminComments /> {/* Render the AdminComments component */}
         </div>
 
         {/* Right Section for Reviews */}
-        <div className="w-full lg:w-1/2  bg-white p-4 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-semibold mb-4">Reviews Section</h2>
+        <div className="w-full lg:w-1/2 p-4 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-semibold mb-4"></h2>
           <AdminReviews /> {/* Render the AdminReviews component */}
         </div>
       </div>
