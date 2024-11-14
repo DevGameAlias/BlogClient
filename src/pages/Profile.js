8f719dd21e8461b00888ccb405f03457154946d8
AUniqueSoulE <aunis87@icloud.com>
2024-11-13 20:45:15 -0500

    added some tailwind css to profile.js code. passing to samuel for more css styling and bloglistbutton importing.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-
-import BlogForm from './BlogForm'; // Import the BlogForm component for submitting new blogs
-import BlogListButton from '../components/BlogListButton'; // Adjust the path if necessary
+import BlogForm from './BlogForm';
 import StorySubmit from "./SubmitStory";
 import EventCreation from "../components/EventCreation.js";
 import StoryCreation from "../components/StoryCreation.js";
 
-// import StoryDelete from "./DeleteStory";
 const Profile = () => {
-  const [isVisible,setIsVisible]= useState(false)
+  const [isVisible, setIsVisible] = useState(false);
   const [blogs, setBlogs] = useState([]);
 
   const addBlog = (blog) => {
@@ -17,53 +14,38 @@ const Profile = () => {
   };
 
   return (
-    <>
-          <div className="profile-page">
-              <div className="blog">
-                  <BlogForm onAddBlog={addBlog} />
-                  {/* List of submitted blogs */}
-              </div>
-              <div className="short-story">
-              {/* <StoryCreation /> Add the StoryCreation component here */}
-                  {/* Insert Short story CRUD here */}
-              </div>
-              <div className="blog-list">
-                  <BlogListButton /> {/* Add the BlogListButton component here */}
-              </div>
-          </div>
-  {/* // Short story component that needs to be moved into components and called into profile    */}
-  
-    <div className="blog">{/* Insertn Blog Crud here */}</div>
-<button className='bg-blue-500 text-white p-2 rounded hover:animate-pulse '
-onClick={()=>{
-  setIsVisible(true)
-}}>
+    <div className="container mx-auto p-4">
+      <div className="grid grid-cols-2 gap-4">
+        {/* Left Column - Blog Section */}
+        <div className="space-y-4">
+          <BlogForm onAddBlog={addBlog} />
+        </div>
+
+        {/* Right Column - Story and Event Section */}
+        <div className="space-y-4">
+          <button 
+            className="bg-amber-800 hover:bg-amber-900 text-white px-4 py-2 rounded transition-colors duration-200"
+            onClick={() => setIsVisible(true)}
+          >
             Create Story
           </button>
-        { isVisible && 
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-          <div className="bg-white p-6 rounded-lg shadow-lg max-w-lg w-full relative z-50 ">
-            <StoryCreation setIsVisible={setIsVisible}/> Add the StoryCreation component here
-            {/* Insert Short story CRUD here */}
+          
+          <div className="event-creation">
+            <EventCreation />
           </div>
         </div>
-        }
+      </div>
 
-          {/* EventCreation component */}
-        <div className="event-creation">
-          <EventCreation />
+      {/* Story Creation Modal */}
+      {isVisible && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="bg-amber-800 hover:bg-amber-900 text-white px-4 py-2 rounded transition-colors duration-200">
+            <StoryCreation setIsVisible={setIsVisible} />
+          </div>
         </div>
-        <div className="blog-list">
-          <BlogListButton /> {/* Add the BlogListButton component here */}
-        </div>
-      
-      {/* // Short story component that needs to be moved into components and called into profile    */}
-      
-      <div className="blog">{/* Insertn Blog Crud here */}</div>
-      
-      
-      </>
-    );
-  };
+      )}
+    </div>
+  );
+};
 
 export default Profile;
