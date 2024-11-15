f13682c3c0b38105900b1a07c60309dc2d515585
Rick <166161351+DevGameAlias@users.noreply.github.com>
2024-11-15 14:05:32 -0500

    add button for bloglist

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, {  useState } from "react";
+import { NavLink ,useNavigate} from "react-router-dom";
 import BlogForm from './BlogForm';
-import BlogList from '../components/BlogListButton.js'
+import BlogList from "./BlogList.js";
 import StorySubmit from "./SubmitStory";
 import EventCreation from "../components/EventCreation.js";
 import StoryCreation from "../components/StoryCreation.js";
@@ -9,6 +9,7 @@ import StoryCreation from "../components/StoryCreation.js";
 const Profile = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [blogs, setBlogs] = useState([]);
+  const navigate = useNavigate();
 
   const addBlog = (blog) => {
     setBlogs([...blogs, blog]);
@@ -18,9 +19,14 @@ const Profile = () => {
     <div className="grid">
       <div className="grid">
         {/* Left Column - Blog Section */}
-        <div className="">
+        <div className="grid bg-gradient-to-r from-orange-100 via-orange-200 to-orange-100">
           <BlogForm onAddBlog={addBlog} />
-        
+          <button 
+            className="grid bg-gradient-to-r from-orange-100 via-orange-200 to-orange-100"
+            onClick={() => navigate('/bloglist')}
+          >
+            Go to BlogList
+          </button>
 
           {/* Right Column - Story and Event Section */}
           <div className="grid bg-gradient-to-r from-orange-100 via-orange-200 to-orange-100">
